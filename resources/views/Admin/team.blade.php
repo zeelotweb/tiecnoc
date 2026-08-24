b8333fd3a5ee09d02784348268b3b148649c6c88
zeelotweb <67663419+zeelotweb@users.noreply.github.com>
2026-08-24 10:16:23 -0400

    Update team.blade.php

--- resources/views/Admin/team.blade.php
+++ resources/views/Admin/team.blade.php
@@ -1,18 +1,8 @@
 <x-layouts::admin>
     <x-slot:pageEyebrow>{{ __('Organization') }}</x-slot:pageEyebrow>
-    <x-slot:pageTitle>{{ __('Team') }}</x-slot:pageTitle>
+    <x-slot:pageTitle>{{ __('Crew') }}</x-slot:pageTitle>
 
-    <div class="space-y-12">
-        @if(auth()->user()->isSuperAdmin())
-            <div class="space-y-4">
-                <p class="text-sm font-medium border-b border-black/15 dark:border-white/15 pb-3">Roles</p>
-                <livewire:admin.super-admin-control />
-            </div>
-        @endif
-
-        <div class="space-y-4">
-            <p class="text-sm font-medium border-b border-black/15 dark:border-white/15 pb-3">Tool Delegation</p>
-            <livewire:admin.delegate-work />
-        </div>
-    </div>
+    @if(auth()->user()->isSuperAdmin())
+        <livewire:admin.super-admin-control />
+    @endif
 </x-layouts::admin>
